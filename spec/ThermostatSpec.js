771aadc4680e77fa09314bf0f53cf55342d3f07b
Tristan Crossley <trcrossley@gmail.com>
2016-01-07 12:22:37 +0000

    Finished thermostat logic

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -9,4 +9,72 @@ describe("Thermostat", function() {
   it("starts at 20 degrees", function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it("can increase the temperature with the up button", function() {
+    thermostat.tempUp();
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it("can decrease the temperature with the down button", function() {
+    thermostat.tempDown();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it("has a minimum temperature of 10 degrees", function() {
+    thermostat.temperature = 10;
+    expect(function() {
+      thermostat.tempDown();
+    }).toThrow(new Error("minimum temperature exceeded"));
+  });
+
+  it("has a power saving mode that is on by default", function() {
+    expect(thermostat.powerMode).toEqual(true);
+  });
+
+  it("can switch off the power saving mode", function() {
+    thermostat.powerModeSwitch();
+    expect(thermostat.powerMode).toEqual(false);
+  });
+
+  it("can switch on the power saving mode", function() {
+    thermostat.powerModeSwitch();
+    thermostat.powerModeSwitch();
+    expect(thermostat.powerMode).toEqual(true);
+  });
+
+  it("has a maximum temperature of 25 degrees with power saving mode on", function() {
+    thermostat.temperature = 25;
+    expect(function() {
+      thermostat.tempUp();
+    }).toThrow(new Error("maximum power saving temperature reached"));
+  });
+
+  it("has a maximum temperature of 32 degrees with power saving mode off", function() {
+    thermostat.powerModeSwitch();
+    thermostat.temperature = 32;
+    expect(function() {
+      thermostat.tempUp();
+    }).toThrow(new Error("maximum temperature reached"));
+  });
+
+  it("has a reset button that sets the temperature to 20 degrees", function() {
+    thermostat.tempUp();
+    thermostat.resetTemp();
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it("should display low energy usage", function() {
+    thermostat.temperature = 17;
+    expect(thermostat.energyLevel()).toEqual("Low energy usage");
+  });
+
+  it("should display moderate energy usage", function() {
+    thermostat.temperature = 23;
+    expect(thermostat.energyLevel()).toEqual("Moderate energy usage");
+  });
+
+  it("should display high energy usage", function() {
+    thermostat.temperature = 28;
+    expect(thermostat.energyLevel()).toEqual("High energy usage");
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,40 @@
-function Thermostat() {
-  this.temperature = 20;
+function Thermostat(temperature, powerMode) {
+  this.powerMode = powerMode || true;
+  this.temperature = temperature || 20;
+
 }
+
+Thermostat.prototype.tempUp = function() {
+  if (this.powerMode === false && this.temperature >= 32) {
+    throw new Error("maximum temperature reached");
+  }
+  if (this.powerMode === true && this.temperature >= 25) {
+    throw new Error("maximum power saving temperature reached");
+  }
+  return this.temperature++;
+};
+
+Thermostat.prototype.tempDown = function() {
+  if (this.temperature <= 10) {
+    throw new Error("minimum temperature exceeded");
+  }
+  return this.temperature--;
+};
+
+Thermostat.prototype.powerModeSwitch = function() {
+  if (this.powerMode === true) {this.powerMode = false;}
+  else if (this.powerMode === false) {this.powerMode = true;}
+};
+
+Thermostat.prototype.resetTemp = function() {
+  this.temperature = 20;
+};
+
+Thermostat.prototype.energyLevel = function() {
+  if (this.temperature <= 18)
+    return ("Low energy usage");
+  if (this.temperature <= 25)
+    return ("Moderate energy usage");
+  if (this.temperature >= 26)
+    return ("High energy usage");
+};
